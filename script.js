cf436d1104a003077b1e5946c7d81dd979ed2c89
m-wie-mischa <mischa.google@gmx.de>
2025-02-20 12:20:35 +0100

    Updated db logic for output

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const {writeDb} = require('./dataBaseFunc')
+const {writeDb, readDb} = require('./dataBaseFunc')
 
 function onButtonClick() {
     var value = document.getElementById(input).value;
-    var data = {color: value};
+    var data = {text: value};
     writeDb(data);
+    document.getElementById("text").innerHTML = readDb()
 }
